943565f6f26d684396824a78a8c63ed5198a06ce
Erica Millan <ericamillan1234@gmail.com>
2025-10-31 12:02:04 -0500

    adding a 2000's tech web page to github

---
+++ 2000s-tech/WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿ // Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//Adds hover effect to heading that makes it expand when you hover
+
+//finds magenta-heading and put it in variable "heading"
+
+var heading = document.querySelector(".magenta-heading");
+
+//on hover
+heading.onmouseover = function () {
+    //styling as such (expands)
+    heading.style.transform = "scale(1.2)";
+    heading.style.transition = "transform 0.3s ease";
+};
+//off hover, font shrinks.
+heading.onmouseout = function () {
+    heading.style.transform = "scale(1)";
+};
